3bff1fa524d043c761e887a4c293f64a77d09116
jaineil <mandaviajaineil@gmail.com>
2021-11-25 02:30:36 -0800

    refactor (backend): models/added firstName, lastName to employer and job applicant details

--- kafka-backend/src/db/models/mongo/jobSeekerDetails.js
+++ kafka-backend/src/db/models/mongo/jobSeekerDetails.js
@@ -1,63 +1,61 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
 const jobSeekerSchema = new Schema({
-    userId: {type : Number},
-    profilePicture: 
-        { type : String },
-    resumes: [
-        {
-            url: { type : String },
-            name: { type : String }
-        }
-      ],
-    coverLetters: [
-        {
-            url: { type : String },
-            name: { type : String }
-        }
-      ],
-    contactNumber: { type : String },
-    address: {
-        street: { type: String },
-        apt: { type: String },
-        city: { type : String },
-        state: { type : String },
-        country: { type : String },
-        zip: { type : String }
-    },
-    savedJobs: [{
-        jobId: 
-        {   
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "jobDetails"
-        },
-        jobTitle: { type : String },
-        companyId:    
-            {   
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "companyDetails"
-            },
-        companyName: { type : String }
-    }],
-    // reviews: [{
-    //     reviewId:
-    //         {   
-    //             type: mongoose.Schema.Types.ObjectId,
-    //             ref: "reviews"
-    //         },
-    //    companyId: 
-    //    {   
-    //        type: mongoose.Schema.Types.ObjectId,
-    //        ref: "companyDetails"
-    //    }
-    // }]
-
-
-    
+	userId: { type: Number },
+	firstName: { type: String },
+	lastName: { type: String },
+	profilePicture: { type: String },
+	resumes: [
+		{
+			url: { type: String },
+			name: { type: String },
+		},
+	],
+	coverLetters: [
+		{
+			url: { type: String },
+			name: { type: String },
+		},
+	],
+	contactNumber: { type: String },
+	address: {
+		street: { type: String },
+		apt: { type: String },
+		city: { type: String },
+		state: { type: String },
+		country: { type: String },
+		zip: { type: String },
+	},
+	savedJobs: [
+		{
+			jobId: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "jobDetails",
+			},
+			jobTitle: { type: String },
+			companyId: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "companyDetails",
+			},
+			companyName: { type: String },
+		},
+	],
+	// reviews: [{
+	//     reviewId:
+	//         {
+	//             type: mongoose.Schema.Types.ObjectId,
+	//             ref: "reviews"
+	//         },
+	//    companyId:
+	//    {
+	//        type: mongoose.Schema.Types.ObjectId,
+	//        ref: "companyDetails"
+	//    }
+	// }]
 });
 
-const JobSeekerDetails = mongoose.model('jobSeekerDetail', jobSeekerSchema);
+const JobSeekerDetails = mongoose.model("jobSeekerDetail", jobSeekerSchema);
 
 export default JobSeekerDetails;
