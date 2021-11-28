e1e026f6e2228dea506b35de0a561aa5cc6346e9
jaineil <mandaviajaineil@gmail.com>
2021-11-28 01:42:56 -0800

    feat (kafka-backend): search/search jobs by role and location

--- backend/src/modules/search/router/search.js
+++ backend/src/modules/search/router/search.js
@@ -8,6 +8,7 @@ searchRouter.get(
 	"/job-seeker/search-suggestions",
 	search.searchJobsTitlesForAutocomplete
 );
+searchRouter.get("/job-seeker/search-jobs", search.searchJobs);
 searchRouter.get("/job-seeker/search-salary-for-role", search.searchSalaries);
 searchRouter.get("/job-seeker/search-for-companies", search.searchCompanies);
 export default searchRouter;
