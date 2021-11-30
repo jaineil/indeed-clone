d8a60cdfa501897e4fe9cf4cfdadff0574c4ab43
jash-05 <jash.sheth@gmail.com>
2021-11-29 16:49:08 -0800

    refactor (frontend-chats): added base files for job seeker and employer chats pages

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import register from './components/common/Register';
-import home from './components/jobseeker/Landing/Home'
-import login from './components/common/Login';
-import jobs from './components/jobseeker/jobs/DisplayJobs';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import register from "./components/common/Register";
+import home from "./components/jobseeker/Landing/Home";
+import login from "./components/common/Login";
+import jobs from "./components/jobseeker/jobs/DisplayJobs";
 
-import employerDashboard from './components/employer/LandingPage';
-import employerProfile from './components/employer/Profile';
+import employerDashboard from "./components/employer/LandingPage";
+import employerProfile from "./components/employer/Profile";
 
+import JobSeekerChats from "./components/chats/jobSeeker";
+import EmployerChats from "./components/chats/employer";
 class Main extends Component {
-    render() {
-        return (
-            <div>
-                <Route exact path="/" component={register} />
-                <Route path="/login" component={login} />
-                <Route path="/register" component={register} />
-                <Route path="/home" component={home} />
-                <Route path="/displayjobs" component={jobs} />
-                <Route path="/employer" component={employerDashboard} />
-                <Route path="/profile" component={employerProfile} />
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<Route exact path="/" component={register} />
+				<Route path="/login" component={login} />
+				<Route path="/register" component={register} />
+				<Route path="/home" component={home} />
+				<Route path="/displayjobs" component={jobs} />
+				<Route path="/employer" component={employerDashboard} />
+				<Route path="/profile" component={employerProfile} />
+				<Route path="/jobSeekerChats" component={JobSeekerChats} />
+				<Route path="/employerChats" component={EmployerChats} />
+			</div>
+		);
+	}
 }
 export default Main;
---
+++ frontend/src/components/chats/jobSeeker.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "react-chat-elements/dist/main.css";
+import { MessageBox } from "react-chat-elements";
+
+const JobSeekerChats = () => {
+	return <>hi</>;
+};
+
+export default JobSeekerChats;
