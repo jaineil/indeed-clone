a927c7a6e4448033d2f701456422f76f22a81044
jash-05 <jash.sheth@gmail.com>
2021-11-29 17:46:00 -0800

    refactor (chats, backend packages): added chat layout for job seeker

--- frontend/src/components/chats/jobSeeker.js
+++ frontend/src/components/chats/jobSeeker.js
@@ -1,10 +1,130 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "react-chat-elements/dist/main.css";
-import { MessageBox } from "react-chat-elements";
+import { MessageList, ChatList } from "react-chat-elements";
+import { useEffect, useState } from "react";
+import { Container, Button, Form, Row, Col, Card } from "react-bootstrap";
+import Header from "../common/Header";
 
 const JobSeekerChats = () => {
-	return <>hi</>;
+	const [activeChat, setActiveChat] = useState("");
+	return (
+		<>
+			<Header />
+			<Container
+				fluid
+				style={{ backgroundColor: "#f6f6f6" }}
+				className="px-5"
+			>
+				<Row>
+					<Col xs={4} className="p-3">
+						<Container
+							className="h-100 d-inline-block"
+							style={{ backgroundColor: "white" }}
+						>
+							<ChatList
+								className="chat-list"
+								dataSource={[
+									{
+										avatar: "https://facebook.github.io/react/img/logo.svg",
+										alt: "Reactjs",
+										title: "Facebook",
+										subtitle: "What are you doing?",
+										date: new Date(),
+										unread: 0,
+									},
+									{
+										avatar: "https://facebook.github.io/react/img/logo.svg",
+										alt: "Reactjs",
+										title: "Facebook",
+										subtitle: "What are you doing?",
+										date: new Date(),
+										unread: 0,
+									},
+									{
+										avatar: "https://facebook.github.io/react/img/logo.svg",
+										alt: "Reactjs",
+										title: "Facebook",
+										subtitle: "What are you doing?",
+										date: new Date(),
+										unread: 0,
+									},
+									{
+										avatar: "https://facebook.github.io/react/img/logo.svg",
+										alt: "Reactjs",
+										title: "Facebook",
+										subtitle: "What are you doing?",
+										date: new Date(),
+										unread: 0,
+									},
+									{
+										avatar: "https://facebook.github.io/react/img/logo.svg",
+										alt: "Reactjs",
+										title: "Facebook",
+										subtitle: "What are you doing?",
+										date: new Date(),
+										unread: 0,
+									},
+									{
+										avatar: "https://facebook.github.io/react/img/logo.svg",
+										alt: "Reactjs",
+										title: "Facebook",
+										subtitle: "What are you doing?",
+										date: new Date(),
+										unread: 0,
+									},
+								]}
+							/>
+						</Container>
+					</Col>
+					<Col xs={8} className="p-3">
+						<Container
+							className="h-100 d-inline-block py-5"
+							style={{ backgroundColor: "white" }}
+						>
+							<MessageList
+								className="message-list"
+								lockable={true}
+								toBottomHeight={"100%"}
+								dataSource={[
+									{
+										position: "left",
+										type: "text",
+										text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+										date: new Date(),
+									},
+									{
+										position: "right",
+										type: "text",
+										text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+										date: new Date(),
+									},
+									{
+										position: "left",
+										type: "text",
+										text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+										date: new Date(),
+									},
+									{
+										position: "right",
+										type: "text",
+										text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+										date: new Date(),
+									},
+									{
+										position: "left",
+										type: "text",
+										text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+										date: new Date(),
+									},
+								]}
+							/>
+						</Container>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
 };
 
 export default JobSeekerChats;
