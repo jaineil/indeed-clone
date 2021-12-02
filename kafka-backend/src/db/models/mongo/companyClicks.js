24cdd3d2c028228a89e14d2fb1e72d159022c40e
jash-05 <jash.sheth@gmail.com>
2021-12-01 21:35:33 -0800

    refactor (companyClicks): added moment module, modified company clicks model

--- kafka-backend/src/db/models/mongo/companyClicks.js
+++ kafka-backend/src/db/models/mongo/companyClicks.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import moment from "moment";
 
 const Schema = mongoose.Schema;
 
 const companyClicksSchema = new Schema({
 	companyId: { type: mongoose.Schema.Types.ObjectId, ref: "companyDetail" },
 	companyName: { type: String },
-	date: { type: Date, default: Date.now },
+	date: { type: String, default: moment().format("MM-DD-YYYY") },
 	clicks: { type: Number, default: 0 },
 });
 
